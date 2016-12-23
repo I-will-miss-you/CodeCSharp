28fb3f0357a957e9f261be2c5275b1dafd05e297
Code36u4r60 <code36u4r60@hotmail.com>
2016-12-23 01:14:56 +0000

    Estudo em CSharp
    Aulas Concluidas:
    1, 2, 3, 4, 5, 6, 7, 8.

---
+++ Microsoft Virtual Academy/Conceitos básicos de C#/Decisions/Decisions/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decisions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Console.WriteLine("Bob's Gig Giveaway");
+            Console.WriteLine("Choose a door 1, 2 or 3: ");
+            string userValue = Console.ReadLine();
+
+            string message = "";
+
+            if (userValue == "1")
+               message = "You won a new car!";
+            else if(userValue == "2")
+               message = "You won a new boat!";
+            else if(userValue == "3")
+               message = "You won a new cat!";
+            else
+            {
+                message = "Sorry, we didn't understand.";
+                message += " You Lose.";
+            }
+
+            Console.WriteLine(message);
+            Console.ReadLine();
+            */
+
+            Console.WriteLine("Bob's Gig Giveaway");
+            Console.WriteLine("Choose a door 1, 2 or 3: ");
+            string userValue = Console.ReadLine();
+
+            string message = (userValue == "1") ? "boat" : "strand of lint";
+            //Console.WriteLine("You won a {0}.",message);
+            Console.WriteLine("You entered {0}, therefore you won a {1}.",userValue,message);
+            Console.ReadLine();
+        }
+    }
+}
